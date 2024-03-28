2c908c79b203a6ab9dfca158b7a7b159b2bc3f37
Guido Sales Calvano <g.salescalvano@mindef.nl>
2024-03-28 13:16:28 +0100

    stuff

--- main.py
+++ main.py
@@ -53,7 +53,19 @@ def bezier_as_quadratic_with_one_t(bezier_points):
     plt.show()
 
 
-def bezier_maxima_minima_y(bezier_points):
+def bezier_pixel_intercepts(bezier_points):
+
+    p_minimum, p_maximum = bezier_maxima_minima(bezier_points)
+
+    intercept_ranges = np.ceil(p_maximum - p_minimum)
+    cummulative_intercept_count = np.cumsum(intercept_ranges, axis=0)
+    intercept_offsets = cummulative_intercept_count - intercept_ranges
+
+    intercept_count = cummulative_intercept_count[-1, :]
+
+    intercept_indices = intercept_offsets + np.arange(intercept_count) % intercept_ranges
+    intercept_values = p_minimum + np.arange(intercept_count) % intercept_ranges
+
     start = bezier_points[0:-1:2, np.newaxis, :]
     middle = bezier_points[1::2, np.newaxis, :]
     end = bezier_points[2::2, np.newaxis, :]
@@ -75,22 +87,56 @@ def bezier_maxima_minima_y(bezier_points):
     b_per_a = b / a
     c_per_a = c / a
 
+
     m = b_per_a / 2
-    u = np.sqrt(m * m - c_per_a)
+    u_squared_with_impossibles = m * m - c_per_a
+    u_squared = u_squared_with_impossibles[u_squared_with_impossibles > 0]
+    u = np.sqrt(u_squared)
 
     t1 = m - u
     t2 = m + u
 
-    t_optimum = (-b_per_a / 2)[:, 0, :, np.newaxis]
 
-    t_bounds = np.clip(t_optimum, 0, 1)
+def bezier_maxima_minima(bezier_points):
+    start = bezier_points[0:-1:2, np.newaxis, :]
+    middle = bezier_points[1::2, np.newaxis, :]
+    end = bezier_points[2::2, np.newaxis, :]
 
-    p_bounds = np.reshape((t_bounds * t_bounds) * (start + end - 2 * middle) - t_bounds * 2 * (start - middle) + 1 * start, [-1, 2])
+    DETAIL = 50
+    t_values_last = (np.arange(DETAIL) / (DETAIL - 1))[np.newaxis, :, np.newaxis]
+    t_values_first = (1 - t_values_last)
+    # p_final = np.reshape(middle + (1 - t_values_last) * (1 - t_values_last) * (start - middle) + t_values_last * t_values_last * (end - middle), [-1, 2])
+    # p_final = np.reshape(middle + (t_values_last * t_values_last - 2 * t_values_last + 1) * (start - middle) + t_values_last * t_values_last * (end - middle), [-1, 2])
+
+    a = (start + end - 2 * middle)
+    b = -2 * (start - middle)
+    c = start
+
+    # derivative for maximum/minimum = 0:
+    # 2 * a * t + b = 0
+    # t_max_or_min = -b / (2 * a)
+
+    b_per_a = b / a
+    c_per_a = c / a
+
+    t_optimum = np.clip(-b_per_a / 2, 0, 1) # [:, 0, :, np.newaxis]
+    p_optimum = (t_optimum * t_optimum) * (start + end - 2 * middle) - t_optimum * 2 * (start - middle) + 1 * start
+    #
+    minimum_p = np.minimum(np.minimum(start, end), p_optimum)[:, 0, :]
+    maximum_p = np.maximum(np.maximum(start, end), p_optimum)[:, 0, :]
+
     p_contour = np.reshape((t_values_last * t_values_last) * (start + end - 2 * middle) - t_values_last * 2 * (start - middle) + 1 * start, [-1, 2])
 
     plt.plot(p_contour[:, 0], p_contour[:, 1])
-    plt.scatter(p_bounds[:, 0], p_bounds[:, 1])
+
+    plt.scatter(minimum_p[:, 0], np.zeros_like(minimum_p[:, 0]), c="green")
+    plt.scatter(maximum_p[:, 0], np.zeros_like(minimum_p[:, 0]), c="red")
+
+    plt.scatter(np.zeros_like(minimum_p[:, 1]), minimum_p[:, 1], c="green")
+    plt.scatter(np.zeros_like(maximum_p[:, 1]), maximum_p[:, 1], c="red")
+
     plt.show()
+    return minimum_p, maximum_p
 
 
 def main():
@@ -116,7 +162,7 @@ def main():
     bezier_points[:, 0] = np.cos(alpha) * radius * 2
     bezier_points[:, 1] = np.sin(alpha) * radius * 2
 
-    bezier_maxima_minima_y(bezier_points)
+    bezier_pixel_intercepts(bezier_points)
 
     pass
 
